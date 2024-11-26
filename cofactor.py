557955e6ca253a7dc763608f634c8f9600c756e9
m45adiwinata <m45adiwinata@gmail.com>
2024-11-27 00:55:11 +0700

    done

--- cofactor.py
+++ cofactor.py
@@ -1,8 +1,15 @@
+# matrix = [
+#     [2, 1, 3, 1],
+#     [1, 0, 1, 1],
+#     [0, 2, 1, 0],
+#     [0, 1, 2, 3]
+# ]
 matrix = [
-    [2, 1, 3, 1],
-    [1, 0, 1, 1],
-    [0, 2, 1, 0],
-    [0, 1, 2, 3]
+    [1,2,2,3,4],
+    [4,9,10,13,17],
+    [2,6,7,10,14],
+    [3,10,20,22,40],
+    [1,5,10,12,16]
 ]
 
 def determinant(arr: list) -> float:
@@ -10,7 +17,6 @@ def determinant(arr: list) -> float:
     return val
 
 def cofactor(arr: list) -> float:
-    positive = True
     val = 0
     pengali = 1
     for i in range(len(arr)):
@@ -21,26 +27,14 @@ def cofactor(arr: list) -> float:
                 if k != i:
                     x.append(arr[j][k])
             arr_det.append(x)
-        det = determinant(arr_det)
+        if len(arr_det) > 2:
+            det = cofactor(arr_det)
+        else:
+            det = determinant(arr_det)
         val += pengali * arr[0][i] * det
         pengali *= -1
     return val
 
-pengali = -1
-val = 0
-for i in range(len(matrix)):
-    pengali *= -1
-    a = matrix[0][i]
-    arr_det = []
-    for j in range(1, len(matrix)):
-        x = []
-        for k in range(len(matrix)):
-            if k != i:
-                x.append(matrix[j][k])
-        arr_det.append(x)
-    print(arr_det)
-    c = cofactor(arr_det)
-    print("a = %d c = %d" % (a, c))
-    val += pengali * a * c
+val = cofactor(matrix)
 
 print(val)
